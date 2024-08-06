df62ecc59b710d4534a00257b6ba82dcf98ad323
Agung Prema <agungprema71@gmail.com>
2024-08-06 10:18:25 +0800

    nextconfig change

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
     images: {
-      domains: ["lh3.googleusercontent.com", "s3.us-west-2.amazonaws.com"],
+        domains: ["lh3.googleusercontent.com", "s3.us-west-2.amazonaws.com"],
     },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    output: "standalone",
-  };
-  
-  module.exports = nextConfig;
+};
+
+module.exports = nextConfig;
